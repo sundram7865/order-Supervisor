26da1e75a26828be5426e10efa514301e6d3dbee
Sundram Mishra <ram322222@gmail.com>
2026-07-26 14:40:32 +0530

    chore: initialize backend and frontend projects

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center  font-sans bg-red-800">
+     hi there
+    </div>
+  );
+}
